d027e8cc9fdb314b31d75266df24c1ad8f5b2077
Oleg Smirnov <oleg.smirnov@gmail.com>
2011-12-01 19:14:51 +0200

    Fix streaming examples

--- lab2/run-streaming1.sh
+++ lab2/run-streaming1.sh
@@ -3,7 +3,8 @@
 . /usr/share/hadoop/bin/hadoop-config.sh
 
 $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/hadoop-streaming.jar \
-    -input test1 \
+    -input lorem \
     -output out1 \
+    -inputformat KeyValueTextInputFormat \
     -mapper org.apache.hadoop.mapred.lib.IdentityMapper \
     -reducer org.apache.hadoop.mapred.lib.IdentityReducer
--- lab2/run-streaming2.sh
+++ lab2/run-streaming2.sh
@@ -3,7 +3,7 @@
 . /usr/share/hadoop/bin/hadoop-config.sh
 
 $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/hadoop-streaming.jar \
-    -input test1 \
+    -input lorem \
     -output out2 \
     -mapper /bin/cat \
     -reducer /usr/bin/wc
