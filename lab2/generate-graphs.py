093eb3f42dd19dc1e951268a0c68381050eba4e5
Oleg Smirnov <oleg.smirnov@gmail.com>
2011-12-01 16:17:26 +0200

    Add generators for BA & WS graphs

--- lab2/generate-graphs.py
+++ lab2/generate-graphs.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 import networkx as nx
 
-def generate(nodes, conn, name):
-    # As per Duncan J. Watts and Steven H. Strogatz, Collective dynamics
-    # of small-world networks, Nature, 393, pp. 440-442, 1998.
-    G = nx.generators.connected_watts_strogatz_graph(nodes, conn, 0.3)
+def save(G, name):
     f = open("%s.txt" % name, "w")
     for v in G.nodes_iter():
         neighbors = " ".join([str(w) for w in G.neighbors_iter(v)])
@@ -13,6 +11,19 @@ def generate(nodes, conn, name):
     nx.write_dot(G, "%s.dot" % name)
 
 if __name__ == "__main__":
-    generate(100, 10, "small-100")
-    # 120 is Dunbar's number
-    generate(10000, 120, "large-10k")
+    # As per Duncan J. Watts and Steven H. Strogatz, Collective dynamics
+    # of small-world networks, Nature, 393, pp. 440-442, 1998.
+    G = nx.generators.connected_watts_strogatz_graph(100, 10, 0.3)
+    save(G, "ws_100_10")
+
+    # 120 is Dunbar's number    
+    G = nx.generators.connected_watts_strogatz_graph(10000, 120, 0.3)
+    save(G, "ws_10k_120")
+
+    # As per A. L. Barabási and R. Albert “Emergence of scaling in random
+    # networks”, Science 286, pp 509-512, 1999.
+    G = nx.barabasi_albert_graph(100, 10)
+    save(G, "ba_100_10")
+
+    G = nx.barabasi_albert_graph(40000, 120)
+    save(G, "ba_40k_120")    
